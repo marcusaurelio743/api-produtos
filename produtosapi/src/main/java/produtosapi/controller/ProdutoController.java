0c32c76019aa48cfbb816ed2103203ea79a34207
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-06-22 20:02:30 -0300

    criando o end point produto persistindo os dados

--- produtosapi/src/main/java/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/produtosapi/controller/ProdutoController.java
@@ -1,6 +1,10 @@
 package produtosapi.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +31,27 @@ public class ProdutoController {
 		produtoRepository.save(produto);
 		return produto;
 	}
+	
+	@GetMapping("/{id}")
+	public Produto buscaPorId(@PathVariable("id") Long id) {
+		
+		return produtoRepository.findById(id).orElse(null);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletaPorId(@PathVariable("id") Long id) {
+		produtoRepository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public Produto atualizaPorId(@PathVariable("id") Long id, @RequestBody Produto produto) {
+		Produto obj = produtoRepository.findById(id).get();
+		
+		obj.setNome(produto.getNome());
+		obj.setPreco(produto.getPreco());
+		obj.setDescricao(produto.getDescricao());
+		
+		return produtoRepository.save(obj);
+	}
+	
 }
